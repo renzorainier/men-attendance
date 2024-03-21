735be1381730cd56210a17a54dcdf8dcafdb060e
renzorainier <114910204+renzorainier@users.noreply.github.com>
2024-03-21 15:17:24 +0000

    Thu, Mar 21, 2024, 3:17 PM +00:00

---
+++ src/app/ChildComponent.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function ChildComponent(props) {
+  // Further processing of the memberNames array
+  const processedNames = props.memberNames.map(name => name.toUpperCase());
+
+  return (
+    <div>
+      <h2>Processed Member Names:</h2>
+      <ul>
+        {processedNames.map((name, index) => (
+          <li key={index}> {name} </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default ChildComponent;
