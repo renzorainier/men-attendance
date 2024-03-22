525f5bca3dddec4131e088fbe636aa0b4e543ab2
renzorainier <114910204+renzorainier@users.noreply.github.com>
2024-03-22 06:10:05 +0000

    Fri, Mar 22, 2024, 6:10 AM +00:00

--- src/app/Upload.jsx
+++ src/app/Upload.jsx
@@ -3,39 +3,16 @@ import { doc, getDoc } from "firebase/firestore";
 import { db } from "./firebase.js";
 import Buttons from './Buttons.jsx'; // Assuming ChildComponent is in the same directory
 
-function Fetch() {
-  const [memberNames, setMemberNames] = useState([]);
+function Upload() {
+
 
   useEffect(() => {
-    const fetchNames = async () => {
-      try {
-        const wordRef = doc(db, "men", "members");  // Adjust path if needed
-        const wordDoc = await getDoc(wordRef);
 
-        if (wordDoc.exists()) {
-          const data = wordDoc.data();
-          setMemberNames(data.names);
-          console.log(data);
-        } else {
-          console.log("No such document!");
-        }
-      } catch (error) {
-        console.error("Error fetching data from Firestore:", error);
-      }
-    };
-
-    fetchNames();
   }, []);
 
   return (
-    <div>
-      {memberNames.length > 0 ? (
-        <Buttons memberNames={memberNames} />  // Pass data as prop
-      ) : (
-        <div>Loading names...</div>
-      )}
-    </div>
+
   );
 }
 
-export default Fetch;
+export default Upload;
