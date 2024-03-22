062d066d3e7a889cdfa61e929c7d1d3bc5e07a37
renzorainier <114910204+renzorainier@users.noreply.github.com>
2024-03-22 06:13:11 +0000

    Fri, Mar 22, 2024, 6:13 AM +00:00

--- src/app/Main.jsx
+++ src/app/Main.jsx
@@ -1,16 +1,19 @@
-'use client'
+"use client";
 
-import React from 'react';
-import Fetch from './Fetch';
+import React from "react";
+import Fetch from "./Fetch";
+import Upload from "./Upload";
+
 // import { doc, getDoc } from "/firebase/firestore";
 // import { db } from "./firebase.js";
 
-
 function Main() {
-
-    return (
-        <Fetch/>
-    );
+  return (
+    <div>
+      <Fetch />
+      <Upload />
+    </div>
+  );
 }
 
 export default Main;
--- src/app/Upload.jsx
+++ src/app/Upload.jsx
@@ -1,17 +1,13 @@
 import React, { useState, useEffect } from "react";
 import { doc, getDoc } from "firebase/firestore";
 import { db } from "./firebase.js";
-import Buttons from './Buttons.jsx'; // Assuming ChildComponent is in the same directory
 
 function Upload() {
 
-
-  useEffect(() => {
-
-  }, []);
-
   return (
-
+    <div>
+        hello
+    </div>
   );
 }
 
