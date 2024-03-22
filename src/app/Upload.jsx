fafb22e131fb6b0dfdc1bb26b0f0d193d314112c
renzorainier <114910204+renzorainier@users.noreply.github.com>
2024-03-22 06:15:25 +0000

    Fri, Mar 22, 2024, 6:15 AM +00:00

--- src/app/Upload.jsx
+++ src/app/Upload.jsx
@@ -1,12 +1,25 @@
 import React, { useState, useEffect } from "react";
-import { doc, getDoc } from "firebase/firestore";
+import { collection, addDoc } from "firebase/firestore";
 import { db } from "./firebase.js";
 
 function Upload() {
+  const [names, setNames] = useState([]);
+
+  const uploadToFirestore = async () => {
+    try {
+      const docRef = await addDoc(collection(db, "memberRecords"), {
+        names: names
+      });
+      console.log("Document uploaded with ID: ", docRef.id);
+      setNames([]);
+    } catch (error) {
+      console.error("Error uploading document: ", error);
+    }
+  };
 
   return (
     <div>
-        hello
+      <button onClick={uploadToFirestore}>Upload to Firestore</button>
     </div>
   );
 }
