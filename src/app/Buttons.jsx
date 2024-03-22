3f7e613f9bad81ce7e09a5d510f9d940949d898e
renzorainier <114910204+renzorainier@users.noreply.github.com>
2024-03-22 05:14:36 +0000

    Fri, Mar 22, 2024, 5:14 AM +00:00

--- src/app/Buttons.jsx
+++ src/app/Buttons.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 function Buttons(props) {
   const [selectedNames, setSelectedNames] = useState([]);
@@ -20,30 +20,36 @@ function Buttons(props) {
 
   return (
     <div className="">
-     <h2><span class="text-2xl font-semibold bg-gray-100 p-2 rounded-md shadow-sm mb-4">Members:</span></h2>
+      <h2>
+        <span class="text-2xl font-semibold bg-gray-100 p-2 rounded-md shadow-sm mb-4">
+          Members:
+        </span>
+      </h2>
       <div className="flex flex-col gap-2">
         {sortedMemberNames.map((name, index) => (
           <button
             key={index}
             className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded-xl
-              ${selectedNames.includes(name) ? 'bg-gray-500' : ''}
+              ${selectedNames.includes(name) ? "bg-gray-500" : ""}
               text-lg sm:text-xl md:text-2xl`}
-            onClick={() => handleClick(name)}
-          >
+            onClick={() => handleClick(name)}>
             {name}
           </button>
         ))}
+        <div className="flex gap-2">
+          <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-3 px-6 rounded-xl">
+            Button 1
+          </button>
+          <button className="bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-3 px-6 rounded-xl">
+            Button 2
+          </button>
+        </div>
       </div>
-
     </div>
   );
 }
 
 export default Buttons;
-
-
-
-
 
 // {selectedNames.length > 0 && (
 //   <>
