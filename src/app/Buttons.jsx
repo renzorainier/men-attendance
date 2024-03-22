975ca29aa59150d099efd18f13454d9b751e9e65
renzorainier <114910204+renzorainier@users.noreply.github.com>
2024-03-22 03:39:46 +0000

    Fri, Mar 22, 2024, 3:39 AM +00:00

--- src/app/Buttons.jsx
+++ src/app/Buttons.jsx
@@ -4,17 +4,29 @@ function Buttons(props) {
   const [selectedNames, setSelectedNames] = useState([]);
 
   const handleClick = (name) => {
-    setSelectedNames([...selectedNames, name]);
+    const nameIndex = selectedNames.indexOf(name);
+
+   if (nameIndex === -1) {
+      // Name not in the array, add it
+      setSelectedNames([...selectedNames, name]);
+    } else {
+      // Name already exists, remove it
+      setSelectedNames([
+        ...selectedNames.slice(0, nameIndex),
+        ...selectedNames.slice(nameIndex + 1)
+      ]);
+    }
   };
 
   return (
     <div>
       <h2>Member Buttons:</h2>
-      <div className="flex flex-wrap gap-2"> {/* Container for buttons */}
+      <div className="flex flex-wrap gap-2">
         {props.memberNames.map((name, index) => (
           <button
             key={index}
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded
+              ${selectedNames.includes(name) ? 'bg-gray-500' : ''}`} // Apply a style change if selected
             onClick={() => handleClick(name)}
           >
             {name}
