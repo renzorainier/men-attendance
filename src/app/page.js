dc55159e58fa9fb4bca61d026e678d466a0a9a82
renzorainier <114910204+renzorainier@users.noreply.github.com>
2024-03-23 05:18:06 +0000

    Sat, Mar 23, 2024, 5:18 AM +00:00

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import Main from "./Main.jsx"
 
 export default function Home() {
   return (
-    <main className="">
+    <main className=""style={{ maxWidth: "90%" }}>
       <Main/>
     </main>
   );
