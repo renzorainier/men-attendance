35dc455b384d0280462434749fa56b10390e38b5
renzorainier <114910204+renzorainier@users.noreply.github.com>
2024-03-23 06:52:58 +0000

    Sat, Mar 23, 2024, 6:52 AM +00:00

--- src/app/Members.jsx
+++ src/app/Members.jsx
@@ -89,16 +89,22 @@ function Members() {
   return (
     <div className="flex flex-col items-center">
       <div className="flex flex-col gap-2 w-full">
-        {memberNames.map((name, index) => (
-          <button
-            key={index}
-            className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-4 rounded-xl
-              ${selectedNames.includes(name) ? "bg-gray-500" : ""}
-              text-lg sm:text-xl md:text-2xl`}
-            onClick={() => handleClick(name)}>
-            {name}
-          </button>
-        ))}
+      {memberNames.map((name, index) => {
+          const member = memberData.find((m) => m.id === name);
+
+          return (
+            <button
+              key={index}
+              className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-4 rounded-xl
+                    ${selectedNames.includes(name) ? "bg-gray-500" : ""}
+                    ${member && member[currentWeekNumber] ? "bg-green-500" : ""}
+                    text-lg sm:text-xl md:text-2xl`}
+              onClick={() => handleClick(name)}
+            >
+              {name}
+            </button>
+          );
+        })}
         {selectedNames.length > 0 && (
           <>
             <h3>Selected Names:</h3>
