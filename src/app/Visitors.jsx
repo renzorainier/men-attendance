105927223aa9ea6f4d9562991a680d055ceb9932
renzorainier <114910204+renzorainier@users.noreply.github.com>
2024-03-23 06:29:42 +0000

    Sat, Mar 23, 2024, 6:29 AM +00:00

--- src/app/Visitors.jsx
+++ src/app/Visitors.jsx
@@ -1,5 +1,11 @@
 import React, { useState, useEffect } from "react";
-import { doc, updateDoc, getDocs, collection, setDoc } from "firebase/firestore";
+import {
+  doc,
+  updateDoc,
+  getDocs,
+  collection,
+  setDoc,
+} from "firebase/firestore";
 import { db } from "./firebase.js"; // Assuming you have your Firebase setup
 
 function Visitors() {
@@ -15,16 +21,23 @@ function Visitors() {
   useEffect(() => {
     const fetchVisitors = async () => {
       const visitorsSnapshot = await getDocs(collection(db, "visitors"));
-      const visitorList = visitorsSnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+      const visitorList = visitorsSnapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
 
       // Filter recent and older visitors
       const recentWeeksThreshold = currentWeekNumber - 4;
       const recentVisitors = visitorList.filter((visitor) => {
-        const highestWeek = Math.max(...Object.keys(visitor).filter(key => !isNaN(key)));
+        const highestWeek = Math.max(
+          ...Object.keys(visitor).filter((key) => !isNaN(key))
+        );
         return highestWeek >= recentWeeksThreshold;
       });
       const olderVisitors = visitorList.filter((visitor) => {
-        const highestWeek = Math.max(...Object.keys(visitor).filter(key => !isNaN(key)));
+        const highestWeek = Math.max(
+          ...Object.keys(visitor).filter((key) => !isNaN(key))
+        );
         return highestWeek < recentWeeksThreshold;
       });
 
@@ -105,13 +118,20 @@ function Visitors() {
     return () => clearInterval(intervalId);
   }, []);
 
-
   function getWeekNumber() {
     const date = new Date();
     date.setHours(0, 0, 0, 0);
-    date.setDate(date.getDate() + 3 - (date.getDay() + 6) % 7);
+    date.setDate(date.getDate() + 3 - ((date.getDay() + 6) % 7));
     var week1 = new Date(date.getFullYear(), 0, 4);
-    return 1 + Math.round(((date.getTime() - week1.getTime()) / 86400000 - 3 + (week1.getDay() + 6) % 7) / 7);
+    return (
+      1 +
+      Math.round(
+        ((date.getTime() - week1.getTime()) / 86400000 -
+          3 +
+          ((week1.getDay() + 6) % 7)) /
+          7
+      )
+    );
   }
   // Rendering Section
   return (
@@ -123,9 +143,10 @@ function Visitors() {
             <button
               key={visitor.id}
               className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-4 rounded-xl text-lg sm:text-xl md:text-2xl
-                ${selectedVisitors.includes(visitor.name) ? "bg-gray-500" : ""}`}
-              onClick={() => handleVisitorClick(visitor.name)}
-            >
+                ${
+                  selectedVisitors.includes(visitor.name) ? "bg-gray-500" : ""
+                }`}
+              onClick={() => handleVisitorClick(visitor.name)}>
               {visitor.name}
             </button>
           ))}
@@ -138,37 +159,35 @@ function Visitors() {
             <button
               key={visitor.id}
               className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-4 rounded-xl text-lg sm:text-xl md:text-2xl
-                ${selectedVisitors.includes(visitor.name) ? "bg-gray-500" : ""}`}
-              onClick={() => handleVisitorClick(visitor.name)}
-            >
+                ${
+                  selectedVisitors.includes(visitor.name) ? "bg-gray-500" : ""
+                }`}
+              onClick={() => handleVisitorClick(visitor.name)}>
               {visitor.name}
             </button>
           ))}
         </div>
       </div>
 
-  <div className="flex flex-col items-center gap-2 justify-center mt-6 bg-gray-100 border border-gray-300 rounded-md p-4">
-  <input
-    type="text"
-    value={newVisitorName}
-    onChange={handleInputChange}
-    placeholder="Enter visitor name"
-    className="border border-gray-400 rounded p-3 w-64"
-  />
-  <button
-    className="bg-green-500 hover:bg-green-700 text-white font-bold py-3 px-4 rounded mx-2"
-    onClick={addVisitor}
-  >
-    Add Visitor
-  </button>
-</div>
-<button
-    className="bg-green-500 hover:bg-green-700 text-white font-bold py-3 px-4 rounded mx-2"
-    onClick={updateFirebase}
-  >
-    Button to Upload to Firebase
-  </button>
-
+      <div className="flex flex-col items-center gap-2 justify-center mt-6 bg-gray-100 border border-gray-300 rounded-md p-4">
+        <input
+          type="text"
+          value={newVisitorName}
+          onChange={handleInputChange}
+          placeholder="Enter visitor name"
+          className="border border-gray-400 rounded p-3 w-64"
+        />
+        <button
+          className="bg-green-500 hover:bg-green-700 text-white font-bold py-3 px-4 rounded mx-2"
+          onClick={addVisitor}>
+          Add Visitor
+        </button>
+      </div>
+      <button
+        className="bg-green-500 hover:bg-green-700 text-white font-bold py-3 px-4 rounded mx-2"
+        onClick={updateFirebase}>
+        Button to Upload to Firebase
+      </button>
     </div>
   );
 }
